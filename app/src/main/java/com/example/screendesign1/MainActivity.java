90a53a7136c028ea6a172c70c039f60d8cc641f7
saioufe <saioufeyoutube@yahoo.com>
2022-08-13 17:04:29 -0500

    toolsbar

--- app/src/main/java/com/example/screendesign1/MainActivity.java
+++ app/src/main/java/com/example/screendesign1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.screendesign1;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("saif maher");
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
     }
 }
